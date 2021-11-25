d3ae18c865f1aed264191fecad9349c21b8b7b3f
ilshat <revan931@yandex.ru>
2021-11-25 22:23:46 +0500

    added association to entities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :passed_tests
+  has_many :tests, dependent: :destroy
+
   def user_tests(lvl)
     Test.joins('join passed_tests, users on tests.id = passed_tests.test_id and users.id = passed_tests.user_id')
         .where(tests: {level: lvl}, users: {id: self.id})
