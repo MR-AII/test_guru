370f613d7047a5ac49dba4335f174d1fbc8dfd67
ilshat <revan931@yandex.ru>
2021-12-06 20:41:21 +0500

    test_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,31 @@
 class TestsController < ApplicationController
+
   def index
-    render plain: 'All tests'
+    # render plain: 'All tests'
+
+    # render html: '<h1>All tests</h1>'.html_safe
+
+    # render json: { tests:  [Test.all]}
+
+    # render inline: '<p>My favourite launguge is: <%= %[avaJ].reverse! %>!</p>'
+
+    # render file: 'public/about.html', layout: false
+
+    # head :no_content
+
+    # byebug
+
+    # render inline: '<%= console %>'
+
+    logger.info(self.object_id)
+
+    respond_to do | format |
+      format.html { render plain: 'All Tests' }
+      format.json { render json: { tests: Test.all } }
+    end
   end
 
-  def start
-    render plain: 'Start certain test'
+  def show
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,45 @@
 Rails.application.routes.draw do
-  root :to => 'tests#index'
 
-  get '/tests/:id/start', to: 'tests#start'
+  root to: 'tests#index'
+
+  resources :tests
+
+  # resource :account, controller: :account
+  #
+  # resources :tests, only: %i[index show]
+  #
+  # resources :tests do
+  #   resources :questions
+  # end
+
+  # resources :tests do
+  #   resources :questions, shallow: true
+  # end
+  #
+  # GET /tests/beginner
+  # POST /tests/1/start
+
+  # resources :tests do
+  #   get :beginner, on: :collection
+  #   get :begin, on: :member
+  #
+  #   member do
+  #     get :start
+  #   end
+  # end
+
+  # get '/tests', to: 'tests#index'
+
+  #g et 'tests/:id', to: 'tests#show', as: :test
+
+  # get 'tests(/:id)', to: 'tests#show'
+
+  # get '/tests/:id/start', to: 'tests#start'
+
+  #  get 'tests/:id/start', to: 'tasks#/start'
+
+  # match 'tests/:id', to: 'tests#create', via: %i[post put]
+
+  # # match 'tests/:id', to: 'tests#create', via: :all
 
 end
