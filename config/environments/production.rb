3d6d2706d16a108b8a2cd23f0142c2fdef0b0ba1
BanForName <revan931@yandex.ru>
2022-02-01 09:56:32 +0500

    mailer setting to yandex smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'gentle-beyond-17907.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:          'smtp.gmail.com',
+    address:          'smtp.yandex.ru',
     port:             587,
+    domain:           'yandex.ru',
+    authentication:   'plain',
     user_name:        ENV['SMTP_USERNAME'],
     password:         ENV['SMTP_PASSWORD'],
-    authentication:   'plain',
-    enable_starttls_auto: true }
+    enable_starttls_auto: true,
+    tls:              true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
